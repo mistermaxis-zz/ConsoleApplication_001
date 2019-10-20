3b99c5ee4a6c20ea7cd1f3f0ddce11145e2ce623
mistermaxis <55718118+mistermaxis@users.noreply.github.com>
2019-10-19 22:55:51 -0300

    New Test. Just playing with stuff. Did an ideal weight calculator with the Hamwi formula.

--- ConsoleApplication_001/ConsoleApplication_001.cpp
+++ ConsoleApplication_001/ConsoleApplication_001.cpp
@@ -1,34 +1,96 @@
-// ConsoleApplication_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <iomanip>
+#include <ostream>
 #include <functional>
 #include <string>
-#include "ConsoleApplication_001.h"
 
-class trick {
-public:
-	int& multiply(int&& b, const int&& c) { b *= c; return b; }
+constexpr float HamwiMultiplierMale = 2.7f;
+constexpr float HamwiWeightFactorMale = 48.0f;
+constexpr float HamwiMultiplierFemale = 2.2f;
+constexpr float HamwiWeightFactorFemale = 45.5f;
+constexpr float MetersToInches = 39.37008f;
+constexpr float FiveFeetInInches = 60.0f;
+
+enum class Gender {
+	Male, Female
 };
+
+class Weight {
+private:
+	long double Kilograms;
+public:
+	Weight(long double&& kilos) noexcept : Kilograms(kilos) {}
+	Weight(long double& kilos) noexcept : Kilograms(kilos) {}
+
+	long double getKilograms() const { return Kilograms; }
+};
+
+class Height {
+private:
+	long double Meters;
+public:
+	Height(long double&& meters) noexcept : Meters(meters) {}
+	Height(long double& meters) noexcept : Meters(meters) {}
+
+	long double getMeters() const { return Meters; }
+};
+
+std::ostream& operator << (std::ostream& outstream, Weight& weight) { outstream << weight.getKilograms(); return outstream; }
+std::ostream& operator << (std::ostream& outstream, Height& height) { outstream << height.getMeters(); return outstream; }
+
+Weight&& operator"" _kgs(long double kgs) { return std::move(Weight(kgs)); }
+Height&& operator"" _mts(long double mts) { return std::move(Height(mts)); }
+
+Weight HamwiIdealWeight(const Height height_in_meters, const Gender& gender)
+{
+	Weight result_weight(0.0_kgs);
+	long double ideal_weight = (height_in_meters.getMeters() * MetersToInches) - FiveFeetInInches;
+	
+	switch (gender)
+	{
+		case Gender::Male:
+			ideal_weight *= HamwiMultiplierMale;
+			ideal_weight += HamwiWeightFactorMale;
+			result_weight = ideal_weight;
+			return result_weight;
+
+		case Gender::Female:
+			ideal_weight *= HamwiMultiplierFemale;
+			ideal_weight += HamwiWeightFactorFemale;
+			result_weight = ideal_weight;
+			return result_weight;
+	}
+}
 
 int main()
 {
-	trick t;
-	int x = 4;
-	int &a = t.multiply(std::move(x), 8);
-	a += 3;
-	x += 10;
-	std::cout << &a << " " << &x;
+	long double height_input;
+	char gender_input;
+	std::cout << "Enter you height in meters (eg: 1.45) and gender to receive your ideal weight.\n";
+	std::cout << "Enter your height:\n";
+	std::cin >> height_input;
+	std::cout << "Enter you gender (f for female and m for male):\n";
+	std::cin >> gender_input;
+
+	Height myHeight(height_input);
+	Gender myGender;
+	
+	switch (gender_input)
+	{
+	case 'f':
+		myGender = Gender::Female;
+		break;
+	case 'm':
+		myGender = Gender::Male;
+		break;
+	default:
+		std::cout << "That's not a valid option.\n";
+		break;
+	}
+
+	Weight myIdealWeight = HamwiIdealWeight(myHeight, myGender);
+
+	std::cout << std::setprecision(4) << myIdealWeight << "\n";
+
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
